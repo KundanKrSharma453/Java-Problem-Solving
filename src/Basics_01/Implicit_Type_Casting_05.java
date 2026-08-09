64a7b7679fbbe7c41af1f8939c79a2cabbbf319e
KUNDAN KUMAR SHARMA <207453385+kundankrsharma65@users.noreply.github.com>
2026-08-10 00:17:29 +0530

    Completed upto Implicit_Type_Casting_05

--- src/Basics_01/Implicit_Type_Casting_05.java
+++ src/Basics_01/Implicit_Type_Casting_05.java
@@ -1,7 +1,23 @@
 package Basics_01;
 
+import java.util.Scanner;
+
 public class Implicit_Type_Casting_05 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the int value: ");
+        int integar = sc.nextInt();
+        System.out.print("Enter the float: ");
+        float floating = sc.nextFloat();
 
+
+        // Sum of Both
+        float finalValue = (float) integar + floating;
+        System.out.println("Sum of both: "+finalValue);
+
+        // Type casting
+        floating = integar;
+
+        System.out.println("Int : " + integar + " is converted to Float : " + floating);
     }
 }
